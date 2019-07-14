7e4c42fbbf9d258a906f47572fd7a82afb293d6b
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2019-07-14 20:16:53 +0300

    Implement let and pretty printing

--- src/heartlang/core.clj
+++ src/heartlang/core.clj
@@ -1,14 +1,11 @@
 (ns heartlang.core
   (:gen-class))
 
-
-;;
-
 (def help
   "
 
 Heartlang is a single-file implementation of Simply Typed Lambda Calculus,
-with Naturals and LISP syntax
+with Naturals and Clojur-ish syntax
 
 Syntax:
 - Naturals:      42
@@ -17,10 +14,9 @@ Syntax:
                  Natural
 - Lambdas:       (fn [argument :- type] body)
 - Function type: (-> Type Type)
-??? - Let:           (let [name definition] body)
-- Addition:      (+ e1 e2)
 - Application:   (some-function argument)
-
+- Let:           (let [name definition] body)
+- Addition:      (+ e1 e2)
 
 ")
 
@@ -37,16 +33,21 @@ Syntax:
                :data {:sym (str expr)}})
     list? (condp = (first expr)
             'fn (let [[arg col typ] (second expr)
-                      _ (assert (= (str col) ":-"))
+                      _ (assert (= col :-))
                       _ (assert (symbol? arg))]
                  {:op :lam
                   :data {:sym (str arg)
                          :typ (parse' typ)
                          :body (parse' (nth expr 2))}})
-            ;; 'let?
-            ;;'+ {:op :add
-            ;;    :data {:a (parse' (nth expr 1))
-            ;;           :b (parse' (nth expr 2))}}
+            'let (let [[arg value] (second expr)
+                       _ (assert (symbol? arg))]
+                   {:op :let
+                    :data {:sym (str arg)
+                           :value (parse' value)
+                           :body (parse' (nth expr 2))}})
+            '+ {:op :add
+                :data {:a (parse' (nth expr 1))
+                       :b (parse' (nth expr 2))}}
             '-> {:op :pi
                  :data {:a (parse' (nth expr 1))
                         :b (parse' (nth expr 2))}}
@@ -59,7 +60,7 @@ Syntax:
                 (recur (rest more)
                        {:op :app
                         :data {:f app
-                               :a (parse' (first more))}}))))))
+                               :a (parse' (first more))}}))))
     (str "ERROR: wasn't able to parse: " expr)))
 
 (defmacro parse [expr]
@@ -78,6 +79,9 @@ Syntax:
 (parse ((fn [f :- (-> Natural Natural)]
           (f 42))
         (fn [x :- Natural] (+ x 1))))
+(parse (let [x 1]
+         (let [y 2]
+           (+ x y))))
 
 
 ;; Typecheck
@@ -113,11 +117,6 @@ Syntax:
     :Type    {:op :Type}
     :Natural {:op :Type}
     :nat     {:op :Natural}
-    :add     {:op :pi
-              :data {:a {:op :Natural}
-                     :b {:op :pi
-                         :data {:a {:op :Natural}
-                                :b {:op :Natural}}}}}
 
     ;; 3. Lambda: if adding x : t1 to context gives e : t2, then the lambda has type x:t1 -> t2
     :lam (let [{:keys [sym typ body]} (:data expr)
@@ -128,8 +127,6 @@ Syntax:
                ;; Maybe pi?
            {:op :pi :data {:a normal-typ :b body-type}})
 
-    ;; How about PI?
-
     ;; 4. Application: f : t1 -> t2 and a : t1 in the context, then (f a) : t2
     :app (let [{:keys [f a]} (:data expr)
                f-typ (typecheck f env ctx)
@@ -138,7 +135,26 @@ Syntax:
              (if (= a-typ (-> f-typ :data :a))
                (-> f-typ :data :b)
                (throw (Exception. (str "Type mismatch: tried to apply function to the wrong type:\n\n" f-typ "\n\n" a-typ))))
-             (throw (Exception. (str "Unsupported function type: " f-typ)))))))
+             (throw (Exception. (str "Unsupported function type: " f-typ)))))
+
+    ;; Extensions
+
+
+    ;; How about pi?
+
+    ;; let is just sugar for a lambda
+    :let (let [{:keys [arg value body]} (:data expr)]
+           (typecheck  env ctx))
+
+    ;; + is Natural -> Natural -> Natural
+    :add (let [{:keys [a b]} (:data expr)
+               a-typ (typecheck a env ctx)
+               b-typ (typecheck b env ctx)]
+           (if (or (not= :Natural (:op a-typ))
+                   (not= :Natural (:op b-typ)))
+             (throw (Exception. (str "Tried to apply + to wrong types: " a-typ b-typ)))
+             {:op :Natural}))))
+
 
 
 ;; Testing typecheck
@@ -157,6 +173,9 @@ Syntax:
 (check (fn [x :- Natural] (+ x 42)))
 (check ((fn [x :- Natural] (+ x 42)) 1))
 
+;; Oops, accidentally implemented System F
+(check (fn [x :- Type] (fn [y :- x] y)))
+
 ;; Shadowing
 (check ((fn [x :- Natural] ((fn [x :- Type] x) Natural)) 42))
 
@@ -170,9 +189,11 @@ Syntax:
 
 (defn normalise [expr env]
   (condp = (:op expr)
+    ;; Just replace a var with the value in the env
     :var (or (env-lookup env (-> expr :data :sym))
              expr)
 
+    ;; Generate fresh names for all bindings
     :lam (let [{:keys [sym typ body]} (:data expr)
                new-sym (str (gensym))
                new-env (env-insert env sym {:op :var
@@ -182,6 +203,7 @@ Syntax:
                    :typ (normalise typ env)
                    :body (normalise body new-env)}})
 
+    ;; Apply lambdas: this is the actual equational operation
     :app (let [{:keys [f a]} (:data expr)
                f' (normalise f env)
                a' (normalise a env)]
@@ -190,11 +212,23 @@ Syntax:
              (let [{:keys [sym typ body]} (:data f')
                    new-env (env-insert env sym a')]
                (normalise body new-env))
-             ;; Builtins case
-             (if (and ))
+             ;; Otherwise we just normalise the bodies
              {:op :app
               :data {:f f'
                      :a a'}}))
+
+    ;; Extensions: natural addition
+    :add (let [{:keys [a b]} (:data expr)
+               a' (normalise a env)
+               b' (normalise b env)]
+           (if (and (= :nat (:op a'))
+                    (= :nat (:op b')))
+             {:op :nat
+              :data {:n (+ (-> a' :data :n)
+                           (-> b' :data :n))}}
+             {:op :add
+              :data {:a a'
+                     :b b'}}))
 
     ;; Constants just fall through
     expr))
@@ -206,7 +240,11 @@ Syntax:
 (defmacro run [expr]
   (let [e (parse' expr)
         _ (typecheck e '() '())]
-    (normalise e '())))
+    (println "")
+    (-> (normalise e '())
+       pretty
+       clojure.pprint/pprint)))
+
 
 (run 42)
 (run x)
@@ -223,11 +261,19 @@ Syntax:
 (run (fn [x :- y] z))
 (run +)
 (run (+ 1 2))
-(run (fn [x :- Natural] (+ x 42)))
-(run ((fn [x :- Natural] (+ x 42)) 1))
+(run (fn [x :- Natural]
+       (+ x 42)))
+(run ((fn [x :- Natural]
+        (+ x 42))
+      1))
+
+(run (fn [x :- Natural] (fn [y :- Natural] (fn [z :- Natural] (+ y z)))))
 
 ;; Shadowing
-(run ((fn [x :- Natural] ((fn [x :- Type] x) Natural)) 42))
+(run ((fn [x :- Natural]
+        ((fn [x :- Type] x)
+         Natural))
+      42))
 
 
 
@@ -238,7 +284,20 @@ Syntax:
 
 ;; Bonus: pretty
 
-
+(defn pretty [expr]
+  (condp = (:op expr)
+    :var (-> expr :data :sym symbol)
+    :Type 'Type
+    :Natural 'Natural
+    :nat (-> expr :data :n)
+    :lam (let [{:keys [sym typ body]} (:data expr)]
+           (list 'fn [(symbol sym) :- (pretty typ)] (pretty body)))
+    :app (let [{:keys [f a]} (:data expr)]
+           (list (pretty f) (pretty a)))
+    :add (let [{:keys [a b]} (:data expr)]
+           (list '+ (pretty a) (pretty b)))
+    :pi (let [{:keys [a b]} (:data expr)]
+          (list '-> (pretty a) (pretty b)))))
 
 
 
